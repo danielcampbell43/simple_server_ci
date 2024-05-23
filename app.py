69117466566c7335a4b846e49b0efc80e87edebc
danielcampbell43 <danielsgiveawayemail@icloud.com>
2024-05-23 10:46:54 +0100

    break tests

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    return "Goodbye, world!"
+
 
 if __name__ == '__main__':
     app.run(
-      debug=True,
-      host="0.0.0.0" # Listen for connections _to_ any server
+        debug=True,
+        host="0.0.0.0"  # Listen for connections _to_ any server
     )
